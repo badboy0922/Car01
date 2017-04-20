f922dde50bd02f26c8442123b6049540822a11a3
badboy0922 <zhuynfox@gmail.com>
2017-04-20 20:38:37 +0800

    第一版
    Signed-off-by: badboy0922 <zhuynfox@gmail.com>

---
+++ Car1/Assets/Scripts/CameraFollowsScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 摄像机的跟随 
+/// 可以显得更加真实
+/// </summary>
+public class CameraFollowsScript : MonoBehaviour {
+	public GameObject car;
+	// 水平方向上摄像头和车的距离
+	public float distance = 8f;
+	// 垂直方向上车和摄像头的距离
+	public float heightDiff = 4f;
+
+	// 角度缓冲系数 当车转向的时候  让摄像头的角度发生改变
+	public float angleDamping = 1.5f;
+	// 摄像头的高度的缓冲系数 车在水平高度上发生变化时候 摄像机的跟随 
+	public float heightDamping = 1f;
+	// 摄像头初始视野范围
+	public float cameraScope = 60f;
+	// 当加速的时候 视野范围的缩放比例
+	public float zoomScale = 1.1f;
+	// 记录车的方向
+	private float carDirection;
+
+	void Update () {
+		CamereFollow ();
+	}
+	void CamereFollow () {
+		// 得到摄像机自身的高度
+		float cameraHight = transform.position.y;
+		// 得到车的高度
+		float carHight = car.transform.position.y + heightDiff;
+		// 在车的高度和摄像机的高度之间去一个插值
+		float lerpHight = Mathf.Lerp (cameraHight, carHight, heightDamping * Time.deltaTime);
+
+		// 得到摄像机在y轴上的角
+		float cameraAngle = transform.eulerAngles.y;
+		// 得到车在y轴上的角 
+		float carAngle = carDirection;
+		// 得到两个角之间的插值
+		float lerpAngle = Mathf.LerpAngle (cameraAngle, carAngle, angleDamping * Time.deltaTime);
+		// 将角度的变化用四元数表示
+		Quaternion changeRotation = Quaternion.Euler (0, lerpAngle, 0);
+		// 让摄像机的位置等于车的位置
+		transform.position = car.transform.position;
+		// 让摄像头和车沿着四元数拉开一定得距离
+		transform.position -= changeRotation * Vector3.forward * distance;
+		// 用一个坐标来存储摄像头的位置
+		Vector3 temp = transform.position;
+		// 让摄像机的高度在车的基础上上升一个距离 (y轴方向)
+		temp.y = lerpHight;
+		// 让摄像头的位置等于改变后的位置
+		transform.position = temp;
+		// 改变摄像头的朝向
+		transform.LookAt (car.transform);
+	}
+	void FixedUpdate () {
+
+		// 得到车的速度
+		Vector3 v = car.GetComponent<Rigidbody>().velocity;
+		// 车当前的朝向
+		//Vector3 faceDirection = car.transform.forward;
+		// 速度和车的方向都是向量 前进的时候 两个向量的点积大于零  后退的时候 夹角为钝角 点积小于零
+		//float dot = Vector3.Dot(v, faceDirection);
+		// 转换成本地坐标
+		Vector3 localV = car.transform.InverseTransformDirection(v);
+		// 车当前的方向
+		float currentAngleY = car.transform.eulerAngles.y;
+		if (localV.z < -0.001) {
+			carDirection = currentAngleY + 180;
+		} else {
+			carDirection = currentAngleY;
+		}
+		// 得到速度的大小
+		float speed = v.magnitude;
+		// 摄像机的视野变化
+		GetComponent<Camera>().fieldOfView = cameraScope + speed * zoomScale;
+	}
+}
---
+++ Car1/Assets/Scripts/CarMoveControllerScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 控制车移动的脚本 
+/// 前轮转向
+/// 后轮移动
+/// </summary>
+public class CarMoveControllerScript : MonoBehaviour {
+	
+	// 声明四个轮子
+	public WheelCollider wheelFL; // 前左 front left
+	public WheelCollider wheelFR; // 前右 front right
+	public WheelCollider wheelRL; // 后左 rear left
+	public WheelCollider wheelRR; // 后右 reat right
+	// 声明一个力矩
+	public float MAXTORQUE;
+	// 低速前轮转向的最大角度
+	public float MaxAngleAtLowSpeed;
+	// 高速前轮转向的最大角度
+	public float MaxAngleAtHigSpeed;
+	//相对于转向角的最大速度
+	private float MAX_VELOCITY_FOR_STEER = 45;
+	// 最大速度
+	public float highSpeed;
+	// 当前速度
+	public float currentSpeed;
+
+	// 记录四个轮子的位置信息
+	public Transform wheelFLTransform;
+	public Transform wheelFRTransform;
+	public Transform wheelRLTransform;
+	public Transform wheelRRTransform;
+
+	// 前轮最开始的角度
+	private float initRotationFL = 0f;
+	private float initRotationFR = 0f;
+
+	void Start () {
+		
+	}
+	void Update () {
+		CarWheelMove ();
+	}
+	void FixedUpdate () {
+		CarMove ();
+	}
+	// 利用力矩让车动起来
+	void CarMove () {
+		float ver = Input.GetAxis ("Vertical");
+		float hor = Input.GetAxis ("Horizontal");
+		// 控制前进
+		// 车速度的大小　
+		currentSpeed = GetComponent<Rigidbody> ().velocity.magnitude;
+		if (currentSpeed < highSpeed) {
+			wheelRL.motorTorque = MAXTORQUE * ver;
+			wheelRR.motorTorque = MAXTORQUE * ver;
+		} else {
+			wheelRL.motorTorque = 0;
+			wheelRR.motorTorque = 0;
+		}
+
+
+		float speedFactor = GetComponent<Rigidbody> ().velocity.magnitude / MAX_VELOCITY_FOR_STEER;
+		float steerFactor = Mathf.Lerp (MaxAngleAtLowSpeed, MaxAngleAtHigSpeed, speedFactor);
+		// 控制方向的偏移角度
+		wheelFL.steerAngle = steerFactor * hor;
+		wheelFR.steerAngle = steerFactor * hor;
+	}
+	void CarWheelMove () {
+		// 前左轮转动
+		initRotationFL += wheelFL.rpm * 360 * Time.deltaTime / 60;
+		initRotationFL = Mathf.Repeat (initRotationFL, 360);
+		AngelMove (wheelFLTransform, initRotationFL, wheelFL.steerAngle);
+		// 前右轮转动
+		initRotationFR += wheelFR.rpm * 360 * Time.deltaTime / 60;
+		initRotationFR = Mathf.Repeat (initRotationFL, 360);
+		AngelMove (wheelFRTransform, initRotationFL, wheelFR.steerAngle);
+
+
+		// wheelFLTransform.Rotate (wheelFL.rpm * 360 * Time.deltaTime / 60, 0, 0);
+		// wheelFRTransform.Rotate (wheelFR.rpm * 360 * Time.deltaTime / 60, 0, 0);
+		wheelRLTransform.Rotate (wheelRL.rpm * 360 * Time.deltaTime / 60, 0, 0);
+		wheelRRTransform.Rotate (wheelRR.rpm * 360 * Time.deltaTime / 60, 0, 0);
+
+	}
+	void AngelMove(Transform trans, float angle, float yAngle) {
+		Vector3 a = new Vector3 (angle, yAngle, 0);
+		trans.localRotation = Quaternion.Euler (a);
+	}
+
+}
